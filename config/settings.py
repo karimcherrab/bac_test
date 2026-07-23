9f188bd7d25d77351d258b47b49f3db5798825a2
uvsq22508737 <cherrabkarim2@gmail.com>
2026-07-23 23:44:35 +0200

    mod

--- config/settings.py
+++ config/settings.py
@@ -104,19 +104,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
-#
-# if False:
-#     DATABASES = {
-#         "default": dj_database_url.parse(
-#             DATABASE_URL,
-#             conn_max_age=600,
-#             conn_health_checks=True,
-#             ssl_require=not DEBUG,
-#         )
-#     }
-# else:
-DATABASES = {
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if False:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=not DEBUG,
+        )
+    }
+else:
+    DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
             "NAME": os.getenv(
